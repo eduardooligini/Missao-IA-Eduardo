6521b49ad5ac0b00644f82d6988ada1bc02e0828
eduardooligini <145785071+eduardooligini@users.noreply.github.com>
2025-10-21 11:55:11 -0300

    21

--- main.js
+++ main.js
@@ -89,10 +89,18 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = " ";
 
 function mostraPergunta(){
+
+    if (atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = " ";
     mostraAlternativas();
 }
 
@@ -100,7 +108,7 @@ function mostraAlternativas(){
     for (const alternativa of perguntaAtual.alternativas){
      const botaoAlternativas = document.createElement("button");
      botaoAlternativas.textContent = alternativa.texto;
-     botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
+     botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
      caixaAlternativas.appendChild(botaoAlternativas);
  }
 }
